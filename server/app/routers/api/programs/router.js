52bc89d60b784e02f89bf88bbb8d719df24ae45c
ETCHART Amandine <amandine.yann.lola@gmail.com>
2024-05-24 11:09:20 +0200

    program

--- server/app/routers/api/programs/router.js
+++ server/app/routers/api/programs/router.js
@@ -2,15 +2,9 @@ const express = require("express");
 
 const router = express.Router();
 
-const { browse, read, add } = require("../../../controllers/programActions");
+const { browse } = require("../../../controllers/programActions");
 
 // Route to get a list of programs
 router.get("/", browse);
-
-// Route to get a specific program by ID
-router.get("/:id", read);
-
-// Route to add a new program
-router.post("/", add);
 
 module.exports = router;
