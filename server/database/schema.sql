9e7da4b7ba3e1d3b75741074273254109f893efa
ETCHART Amandine <amandine.yann.lola@gmail.com>
2024-05-28 14:44:34 +0200

    ajout cartes category program

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,7 +1,7 @@
 create table user (
   id int unsigned primary key auto_increment not null,
   email varchar(255) not null unique,
-  password varchar(255) not null
+  pword varchar(255) not null
 );
 
 create table item (
@@ -10,3 +10,20 @@ create table item (
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
 );
+CREATE TABLE `category` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(255) NOT NULL
+);
+
+-- Création de la table `program`
+CREATE TABLE  `program` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `title` VARCHAR(255) NOT NULL,
+    `synopsis` TEXT NOT NULL,
+    `poster` VARCHAR(255) NOT NULL,
+    `country` VARCHAR(255) NOT NULL,
+    `year` INT NOT NULL,
+    `category_id` INT NOT NULL,
+    FOREIGN KEY (`category_id`) REFERENCES `category`(`id`)
+);
+
